6572ee2fefe6d3a34bf219d052dbb5ac17cc2c17
shreya2057 <shreya2057@gmail.com>
2022-11-11 22:42:25 +0545

    Sign Up form completed

--- website/views.py
+++ website/views.py
@@ -5,3 +5,7 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
 	return render_template('home.html')
+
+@views.route('/docprofile')
+def profile():
+	return render_template('docprofile.html')
