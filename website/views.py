b06b5b47d5c219828b55c15705c248ea4ea3ba22
Dipesh Shrestha <51238569+deep-essence347@users.noreply.github.com>
2022-11-12 16:02:40 +0545

    abcd

--- website/views.py
+++ website/views.py
@@ -1,15 +1,18 @@
 import datetime
 import os
+import pickle
 
 from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required
 from werkzeug.utils import secure_filename
 
-from . import app
-from .models import Scan
+from . import app, db
+from .models import Doctor, Patient, Scan
 
 views = Blueprint('views', __name__)
-
+tb_model = pickle.load(open('TB.pkl','rb'))
+pneumonia_model = pickle.load(open('PN.pkl','rb'))
+tumor_model = pickle.load(open('tumor.pkl','rb'))
 @views.route('/')
 def home():
 	return render_template('home.html',user=current_user)
@@ -30,12 +33,25 @@ def upload_scan():
 	if request.method == 'POST':
 		if request.files:
 			image = request.files['scan']
-			# filename=f'scan-{current_user.name}-{datetime.datetime.now()}{os.path.splitext(image.filename)[1]}'
-			# image.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['SCAN_UPLOAD'],secure_filename(filename)))
+			type = request.form.get('type')
 			uri = upload_image(image, 'scan')
-			new_scan = Scan(uri=uri,user_id=current_user.id)
+			new_scan = Scan(uri=uri,user_id=current_user.id, type = type)
+			db.session.commit()
+			pred = prediction(type, image)
 			flash('Scan successfully uploaded.',category='success')
-	return redirect(url_for('views.home'))
+	return redirect(url_for('views.home'),data=pred)
+
+def prediction(type, image):
+	if type=='Tuberculosis':
+		flash('TB')
+		pred = tb_model.predict(image)
+	elif type=='Brain Tumor':
+		flash('tumor')
+		pred = tumor_model.predit(image)
+	elif type=='Penumonia':
+		flash('pneumonia')
+		pred = pneumonia_model(image)
+	return pred
 
 def upload_image(image,type):
 	filename=f'{type}-{current_user.name}-{datetime.datetime.now()}{os.path.splitext(image.filename)[1]}'
