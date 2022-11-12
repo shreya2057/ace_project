6e99f308fc18bc8ff1325084f7e10ecb18b8e07c
Dipesh Shrestha <51238569+deep-essence347@users.noreply.github.com>
2022-11-13 04:15:26 +0545

    abcd

--- website/views.py
+++ website/views.py
@@ -22,6 +22,17 @@ class_names = ['Glioma','Meningioma','No_Tumor','Pituitary']
 
 model =tf.keras.models.load_model("TUMOR.h5")
 
+# def recommend_doctors(disease):
+# 	expertise = ''
+# 	if disease == 'Tuberculosis' or disease == 'Pneumonia':
+# 		expertise = 'Pulmonologist'
+# 	elif disease =='Glioma' or disease == 'Meningioma' or disease =='Pituitary':
+# 		expertise == 'Neurologist'
+# 	else:
+# 		expertise==False
+# 	if(expertise):
+# 		doctors = Doctor.query.filter_by(expertise == expertise).all()
+# 	return doctors
 
 def test_on_image(img_path):
 	test_image = image.load_img(img_path, target_size=(240,240))
@@ -59,6 +70,24 @@ def card():
 	return render_template('doccard.html',user=current_user, doctors=doctors, docDetails=docDetails)
 
 app.config['SCAN_UPLOAD'] = 'static/images/uploads/scans'
+@views.route("/scan-result",methods=['POST'])
+@login_required
+def scan_result():
+	if request.method == 'POST':
+		if request.files:
+			image = request.files['scan']
+			disease = request.form.get('type')
+			# filename=f'scan-{current_user.name}-{datetime.datetime.now()}{os.path.splitext(image.filename)[1]}'
+			# image.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['SCAN_UPLOAD'],secure_filename(filename)))
+			# uri = upload_image(image, 'scan')
+			basepath = os.path.dirname(__file__)
+			file_path=os.path.join(basepath,app.config['SCAN_UPLOAD'],secure_filename(image.filename))
+			image.save(file_path)
+			preds = test_on_image(file_path)
+			# docs = recommend_doctors(disease)
+			# new_scan = Scan(uri=uri,user_id=current_user.id)
+		return render_template('scan_result.html',user=current_user, result=preds,disease=type)
+
 
 @views.route('/upload-scan',methods=['POST'])
 @login_required
@@ -75,8 +104,8 @@ def upload_scan():
 			preds = test_on_image(file_path)
 			print(preds)
 			# new_scan = Scan(uri=uri,user_id=current_user.id)
-			flash(f'Scan successfully uploaded. {preds}',category='success')
-	return redirect(url_for('views.home'))
+			flash(f'Scan successfully uploaded.',category='success')
+	return redirect(url_for('views.home'),user=current_user)
 
 # def upload_image(image,type):
 # 	filename=f'{type}-{current_user.name}-{datetime.datetime.now()}{os.path.splitext(image.filename)[1]}'
